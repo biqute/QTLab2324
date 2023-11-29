0b60143a6092dd5a7a96c79928ec6fcdcf791cc0
Finardi-FS <fs.finardi@gmail.com>
2023-11-29 10:53:44 +0100

    Aggiornamento

--- PAandQubit/VNA/N99xxClass.py
+++ PAandQubit/VNA/N99xxClass.py
@@ -102,21 +102,21 @@ class N99xx:
 # Metodi di scrittura e lettura in un file HDF5
 
 
-  def r_hdf(self, name: str, name_data: str, nth_data: int):
+  def r_hdf_data(self, name: str, name_gp_data: str, nth_data: int):
     with h5py.File(name, 'r') as f:
-      gp = f[name_data][str(nth_data)]
+      gp = f[name_gp_data][str(nth_data)]
       dic = {}
       for i, k in gp.items():
         dic[i] = k[()]
     return dic
   
 
-  def w_hdf(self, name: str, name_data: str, dataset: dict):             # name = nome file hdf5    # name_data = NA o SA
+  def w_hdf(self, name: str, name_gp_data: str, dataset: dict):             # name = nome file hdf5    # name_gp_data = NA o SA
     with h5py.File(name, 'a') as f:                      # creo file hdf5 di nome tra virgolette e lo apro in modalità a = append
-      if name_data not in f.keys():
-        gp = f.create_group(name_data)
+      if name_gp_data not in f.keys():
+        gp = f.create_group(name_gp_data)
       else:
-        gp = f[name_data]
+        gp = f[name_gp_data]
       gp_data = gp.create_group(str(len(gp.keys())))
       for i, k in dataset.items():
         gp_data.create_dataset(i, data = k)
