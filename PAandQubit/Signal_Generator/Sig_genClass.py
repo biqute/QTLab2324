e8abad62ce778b8d96fec213a2a0529e7b3dd440
Finardi-FS <fs.finardi@gmail.com>
2023-12-16 16:07:00 +0100

    Classes reorganization

--- PAandQubit/Signal_Generator/Sig_genClass.py
+++ PAandQubit/Signal_Generator/Sig_genClass.py
@@ -5,7 +5,7 @@
 # https://www.rohde-schwarz.com/webhelp/SMA100B_HTML_UserManual_en/Content/645d6b33dfb54d9b.htm
 
 # Se si comunica col PC, l'interfaccia sua smette di rispondere. Premere LOCAL sul pannello.
-# LAvoriamo solitamente coi microsecondi
+# Lavoriamo solitamente coi microsecondi
 
 # Pulse Width = larghezza gradino
 #   Pulse delay = piedino sinistro del gradino
@@ -16,25 +16,33 @@
 
 
 
-###################################
-#             Methods             #
-#
-# 01 | __init__         :
-# 02 | get_name         :
-#
-# 
-# Modulation
-# 01 | transition_type
-# 02 | pul_gen_params
-# 03 | pul_gen_mode   
-# 
-# Frequency
-# 01 | RF_freq
-# 
-# Level
-# 01 | RF_onoff                       #
-# 02 | lvl_ampl
-###################################
+#########################################################################################################
+#              Methods               :              Description
+#                                    :
+#       [1. Basic methods]           :
+# 1.1 | __init__ ................... : initializes object attributes in a class.
+# 1.2 | get_name ................... : Returns the instrument identification.
+# 1.3 | reset ...................... : Sets the instrument to a defined default status.
+# 1.4 | clear ...................... : Clear status.
+#                                    :
+#                                    :
+#       [2. Modulation]              :
+# 2.1 | transition_type ............ : Sets the transition mode for the pulse signal.
+# 2.2 | pul_gen_params ............. : Sets the period, delay and width of pulse.
+# 2.3 | pul_gen_mode ............... : Selects the mode for the pulse modulation.
+# 2.4 | pul_trig_mode .............. : Selects a trigger mode for generating the modulation signal.
+# 2.5 | pul_state .................. : Activates pulse modulation (1: ON, 0: OFF).
+#                                    :
+#                                    :
+#       [3. Frequency]               :
+# 3.1 | RF_freq .................... : Sets the frequency of the RF output signal.
+#                                    :
+#                                    :
+#       [4. Level]                   :
+# 4.1 | RF_state ................... : Activates the RF output signal (1: ON, 0: OFF).                  
+# 4.2 | RF_lvl_ampl ................ : Sets the amplitude level of the RF output signal.
+#                                    :
+#########################################################################################################à
 
 
 
@@ -47,6 +55,8 @@ import h5py
 
 class SMA100B:
 
+# 1.1 ----------------------------------------------------------------------------------------------------------------------------------------------- #
+
     def __init__(self, ip: str):
 
         self._resource = None
@@ -57,61 +67,64 @@ class SMA100B:
             rm = pyvisa.ResourceManager()
             self._resource = rm.open_resource(f"tcpip0::{ip}::inst0::INSTR")
             self._connect_success = True
-            print("Connessione riuscita!")
+            print("Connection successful!")
         except pyvisa.Error as e:
-            print(f"Errore durante la connessione: {e}")
+            print(f"Unable to establish a connection: {e}")
         
-
-
+# 1.2 ----------------------------------------------------------------------------------------------------------------------------------------------- #
 
     def get_name(self):
         if self._connect_success:
             print(self._resource.query('*IDN?'))
         else:
-            print("Impossibile eseguire il metodo get_name: nessuna connessione attiva.")
+            print("Error: No active connection.")
         
-
+# 1.3 ----------------------------------------------------------------------------------------------------------------------------------------------- #
 
     def reset(self):
-        self._resource.write('*RST')
-        
+        if self._connect_success:
+            self._resource.write('*RST')
+            time.sleep(self._sleep)
+        else:
+            print("Error: No active connection.")
+
+# 1.4 ----------------------------------------------------------------------------------------------------------------------------------------------- #
+
+    def clear(self):
+        if self._connect_success:
+            self._resource.write('*CLS')     
+            time.sleep(self._sleep)
+        else:
+            print("Error: No active connection.")
 
 
+# [2. Modulation] ----------------------------------------------------------------------------------------------------------------------------------- #
 
-
-    
-
-
-# trigger
-
-
-
-
-
-
-
+# 2.1 ----------------------------------------------------------------------------------------------------------------------------------------------- #
 
     def transition_type(self, trans_type):                                      #così fa solo da fast a smoothed
         if self._connect_success:            
             if trans_type in {'FAST', 'SMO'}:
                 self._resource.write(f'SOUR1:PULM:TTYP {trans_type}')  
             else:
-                print('Error. Write FAST or SMO')    
-            
-        
+                print("Error. Invalid string mode. Write 'FAST' or 'SMO'")    
+        else:
+            print("Error: No active connection.")
 
-    #pagina 603-604
+# 2.2 ----------------------------------------------------------------------------------------------------------------------------------------------- #
+
     def pul_gen_params(self, period, delay, width):                        # magari aggiungere confronto con valori dei range operativi, altrimenti errore.           
         if self._connect_success:
-
             
             self._resource.write(f'SOUR:PULM:PER {period*1e-6}') # micro
             self._resource.write(f'SOUR:PULM:DEL {delay*1e-6}')  # micro                #passo di 5 ns. 1-4 approx a 0, 6-9 approx a 5
             self._resource.write(f'SOUR:PULM:WIDT {width*1e-6}') # micro
 
             #print(self._resource.query('SOUR:PULM:DEL?'))
-            
+        else:
+            print("Error: No active connection.")
 
+# 2.3 ----------------------------------------------------------------------------------------------------------------------------------------------- #
 
     def pul_gen_mode(self, mode):
         if self._connect_success:
@@ -119,40 +132,65 @@ class SMA100B:
                 self._resource.write(f'SOUR:PULM:MODE {mode}')
 
             else:
-                print("Error. Invalid string mode. Write SING DOUB PTR")
-                    
+                print("Error. Invalid string mode. Write 'SING', 'DOUB' or 'PTR'")
+        else:
+            print("Error: No active connection.")
+
+# 2.4 ----------------------------------------------------------------------------------------------------------------------------------------------- #
+
+    def pul_trig_mode(self, mode):
+        if self._connect_success:
+            if mode in {'AUTO', 'EXT', 'EGAT', 'SING', 'ESIN'}:
+                self._resource.write(f'SOUR:PULM:TRIG:MODE {mode}')
+
+            else:
+                print("Error. Invalid string mode. Write 'AUTO', 'EXT', 'EGAT', 'SING' or 'ESIN'")
+        else:
+            print("Error: No active connection.")
+
+# 2.5 ----------------------------------------------------------------------------------------------------------------------------------------------- #
 
     def pul_state(self, mode):
         if self._connect_success:
             self._resource.write(f'SOUR:PULM:STAT {mode}')
-        
+        else:
+            print("Error: No active connection.")            
 
 
+# [3. Frequency] ------------------------------------------------------------------------------------------------------------------------------------#
 
+#3.1 ----------------------------------------------------------------------------------------------------------------------------------------------- #
 
-
-
-
-
-
-    def RF_onoff (self, switch):                                            #accendere o spegnere il segnale
-        if self._connect_success:
-            self._resource.write(f'OUTP:STAT {switch}')
-        
     def RF_freq (self, freq):
         if self._connect_success:
             self._resource.write(f'SOUR:FREQ:CW {freq*1e6}')
-    
-    def lvl_ampl (self, amplitude):
+        else:
+            print("Error: No active connection.")
+
+
+# [4. Level] ---------------------------------------------------------------------------------------------------------------------------------------- #
+
+# 4.1 ----------------------------------------------------------------------------------------------------------------------------------------------- #
+
+    def RF_state (self, switch):                                            # Turn ON or OFF the signal
+        if self._connect_success:
+            self._resource.write(f'OUTP:STAT {switch}')
+        else:
+            print("Error: No active connection.")
+
+# 4.2 ----------------------------------------------------------------------------------------------------------------------------------------------- #
+
+    def RF_lvl_ampl (self, amplitude):
         if self._connect_success:
             self._resource.write(f'SOUR:POW:LEV:IMM:AMPL {amplitude}')
-    
+        else:
+            print("Error: No active connection.")
+
 ##### FUNZIONI DA IMPLEMENTARE????? #####
 
 
 
 ####### MODULATION ######
-#Trigger mode (Molto probabilmente no)
 #pulse_mod_sour? (pulse generator or external)
 #pulse external connector: impedance, polarity, threshold
 
--- PAandQubit/VNA/N99xxClass.py
+++ PAandQubit/VNA/N99xxClass.py
@@ -6,11 +6,12 @@
 # 03 | clear            :
 # 04 | get_data         :
 # 05 | get_name         :
-# 06 | set_mode         :
-# 07 | set_NA_par       :
-# 08 | set_freq_range   :
-# 09 | r_hdf            : 
-# 10 | w_hdf            :
+# 06 | runhold          :
+# 07 | set_mode         :
+# 08 | set_NA_par       :
+# 09 | set_freq_range   :
+# 10 | r_hdf            : 
+# 11 | w_hdf            :
 #                                 #
 ###################################
 
@@ -24,150 +25,138 @@ class N99xx:
 
 # 01 ----------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def __init__(self, ip: str):
+	def __init__(self, ip: str):
 
-    self._risorsa = None
-    self._connessione_riuscita = False
-    self._sleep = 1
+		self._resource = None
+		self._connect_success = False
+		self._sleep = 1
 
-    try:
-      rm = pyvisa.ResourceManager()
-      self._risorsa = rm.open_resource(f"tcpip0::{ip}::inst0::INSTR")
-      self._connessione_riuscita = True
-      print("Connessione riuscita!")
-    except pyvisa.Error as e:
-      print(f"Errore durante la connessione: {e}")
-    return
+		try:
+			rm = pyvisa.ResourceManager()
+			self._resource = rm.open_resource(f"tcpip0::{ip}::inst0::INSTR")
+			self._connect_success = True
+			print("Connection successful!")
+		except pyvisa.Error as e:
+			print(f"Unable to establish a connection: {e}")
 
 # 02 ---------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def reset(self):
-    if self._connessione_riuscita:
-      self._risorsa.write('*RST')     
-      time.sleep(self._sleep)
-    else:
-      print("Impossibile eseguire il metodo reset: nessuna connessione attiva.")
-    return
-
+	def reset(self):
+		if self._connect_success:
+			self._resource.write('*RST')     
+			time.sleep(self._sleep)
+		else:
+			print("Error: No active connection.")
+		
 # 03 ---------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def clear(self):
-    if self._connessione_riuscita:
-      self._risorsa.write('*CLS')     #*CLS   è il reset che svuota la memoria (il buffer), utile se si inchioda
-      time.sleep(self._sleep)
-    else:
-      print("Impossibile eseguire il metodo reset: nessuna connessione attiva.")
-    return
-  
+	def clear(self):
+		if self._connect_success:
+			self._resource.write('*CLS')     #*CLS   è il reset che svuota la memoria (il buffer), utile se si inchioda
+			time.sleep(self._sleep)
+		else:
+			print("Error: No active connection.")
+
 # 04 ---------------------------------------------------------------------------------------------------------------------------------------------- #  
 
-  def get_data(self):
-    if self._connessione_riuscita:
-      data_type = ''
-      NA_flag = False
-      if self._risorsa.query('INST:SEL?') == '"NA"\n':
-        data_type = ' SDATA'
-        NA_flag = True
-      
-      valori = self._risorsa.query(f'TRAC1:DATA?{data_type}')  # pag 767  lista di parte reale e parte immaginaria alternati
-      valori = list(map(float, valori.strip('\n').split(',')))
+	def get_data(self):
+		if self._connect_success:
+			data_type = ''
+			NA_flag = False
+			if self._resource.query('INST:SEL?') == '"NA"\n':
+				data_type = ' SDATA'
+				NA_flag = True
+			
+			valori = self._resource.query(f'TRAC1:DATA?{data_type}')  # pag 767  lista di parte reale e parte immaginaria alternati
+			valori = list(map(float, valori.strip('\n').split(',')))
 
-      if NA_flag:
-        I = np.array(valori[::2])   # parte immaginaria
-        Q = np.array(valori[1::2])  # parte reale
-        f = self._risorsa.query('FREQ:DATA?')
-        f = np.array(list(map(float, f.strip('\n').split(',')))) / 1e9    # per esprimere i valori in GHz 
-        return {'f': f, 'I': I, 'Q': Q}
-      
-      else:
-        return {'scalar': valori}
-    else:
-      print("Impossibile eseguire il metodo get_data: nessuna connessione attiva.")
-      return
+			if NA_flag:
+				I = np.array(valori[::2])   # parte immaginaria
+				Q = np.array(valori[1::2])  # parte reale
+				f = self._resource.query('FREQ:DATA?')
+				f = np.array(list(map(float, f.strip('\n').split(',')))) / 1e9    # per esprimere i valori in GHz 
+				return {'f': f, 'I': I, 'Q': Q}
+			
+			else:
+				return {'scalar': valori}
+		else:
+			print("Error: No active connection.")
 
 # 05 ---------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def get_name(self):
-    if self._connessione_riuscita:
-      print(self._risorsa.query('*IDN?'))
-    else:
-      print("Impossibile eseguire il metodo get_name: nessuna connessione attiva.")
-    return
-  
+	def get_name(self):
+		if self._connect_success:
+			print(self._resource.query('*IDN?'))
+		else:
+			print("Error: No active connection.")
+	
 # 06 ---------------------------------------------------------------------------------------------------------------------------------------------- #
-
-  def runhold(self):                              # pag 419
-    self._risorsa.query('TRIG:HOLD;*OPC?')
-    return
-  
+# DA CORREGGERE
+	def runhold(self):                              # pag 419
+		self._resource.query('TRIG:HOLD;*OPC?')
+		
 # 07 ---------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def set_mode(self, mode):
-    if self._connessione_riuscita:
-      valid_modes = ['NA', 'SA']
-      if mode not in valid_modes:
-        raise ValueError("Modalità non valida. Scegliere NA o SA.")
-      self._risorsa.query(f'INST:SEL "{mode}";*OPC?')
-      time.sleep(self._sleep)                                                 # pausa di un secondo
-    else:
-      print("Impossibile eseguire il metodo set_mode: nessuna connessione attiva.")
-    return
-  
+	def set_mode(self, mode):
+		if self._connect_success:
+			valid_modes = ['NA', 'SA']
+			if mode not in valid_modes:
+				raise ValueError("Modalità non valida. Scegliere NA o SA.")
+			self._resource.query(f'INST:SEL "{mode}";*OPC?')
+			time.sleep(self._sleep)                                                 # pausa di un secondo
+		else:
+			print("Error: No active connection.")
+	
 # 08 ---------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def set_NA_par(self, par: str):
-    # S11 - Forward reflection measurement
-    # S21 - Forward transmission measurement
-    # S12 - Reverse transmission 
-    # S22 - Reverse reflection
-    # A - A receiver measurement
-    # B - B receiver measurement
-    # R1 - Port 1 reference receiver measurement
-    # R2 - Port 2 reference receiver measurement
-    if self._connessione_riuscita:
-      self._risorsa.query(f'CALC:PAR1:DEF {par};*OPC?')
-      time.sleep(self._sleep)
-    else:
-      print("Impossibile eseguire il metodo set_NA_par: nessuna connessione attiva.")
-    return
-  
+	def set_NA_par(self, par: str):
+		# S11 - Forward reflection measurement
+		# S21 - Forward transmission measurement
+		# S12 - Reverse transmission 
+		# S22 - Reverse reflection
+		if self._connect_success:
+			self._resource.query(f'CALC:PAR1:DEF {par};*OPC?')
+			time.sleep(self._sleep)
+		else:
+			print("Error: No active connection.")
+	
 # 09 ---------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def set_freq_range(self, fmin: float, fmax: float):
-    if self._connessione_riuscita:
-      # frequenze in GHz
-      self._risorsa.write(f'FREQ:START {fmin * 1e9}')     # set freq iniziale                    
-      self._risorsa.write(f'FREQ:STOP {fmax * 1e9}')      # set freq finale
-    else:
-      print("Impossibile eseguire il metodo set_NA_par: nessuna connessione attiva.")
-    return
+	def set_freq_range(self, fmin: float, fmax: float):
+		if self._connect_success:
+			# frequenze in GHz
+			self._resource.write(f'FREQ:START {fmin * 1e9}')     # set freq iniziale                    
+			self._resource.write(f'FREQ:STOP {fmax * 1e9}')      # set freq finale
+		else:
+			print("Error: No active connection.")
 
-# 10 ---------------------------------------------------------------------------------------------------------------------------------------------- #     
+		 
 
 
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
 
 # Metodi di scrittura e lettura in un file HDF5
 
-  def r_hdf_data(self, name: str, name_gp_data: str, nth_data: int):
-    with h5py.File(name, 'r') as f:
-      gp = f[name_gp_data][str(nth_data)]
-      dic = {}
-      for i, k in gp.items():
-        dic[i] = k[()]
-    return dic
-  
+# 10 ---------------------------------------------------------------------------------------------------------------------------------------------- #
 
-  def w_hdf(self, name: str, name_gp_data: str, dataset: dict):             # name = nome file hdf5    # name_gp_data = NA o SA
-    with h5py.File(name, 'a') as f:                      # creo file hdf5 di nome tra virgolette e lo apro in modalità a = append
-      if name_gp_data not in f.keys():
-        gp = f.create_group(name_gp_data)
-      else:
-        gp = f[name_gp_data]
-      gp_data = gp.create_group(str(len(gp.keys())))
-      for i, k in dataset.items():
-        gp_data.create_dataset(i, data = k)
-    return
-      # capire per SA quali e quanti dati devo caricare
+	def r_hdf_data(self, name: str, name_gp_data: str, nth_data: int):
+		with h5py.File(name, 'r') as f:
+			gp = f[name_gp_data][str(nth_data)]
+			dic = {}
+			for i, k in gp.items():
+				dic[i] = k[()]
+		return dic
+	
+# 11 ---------------------------------------------------------------------------------------------------------------------------------------------- #  
+
+	def w_hdf(self, name: str, name_gp_data: str, dataset: dict):             # name = nome file hdf5    # name_gp_data = NA o SA
+		with h5py.File(name, 'a') as f:                      # creo file hdf5 di nome tra virgolette e lo apro in modalità a = append
+			if name_gp_data not in f.keys():
+				gp = f.create_group(name_gp_data)
+			else:
+				gp = f[name_gp_data]
+			gp_data = gp.create_group(str(len(gp.keys())))
+			for i, k in dataset.items():
+				gp_data.create_dataset(i, data = k)
 
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
