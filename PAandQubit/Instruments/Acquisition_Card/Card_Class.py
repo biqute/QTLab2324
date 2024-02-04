efb41aeb446127cf2d1394d6dc9f3ed455c6667e
Finardi-FS <fs.finardi@gmail.com>
2024-02-04 16:37:08 +0100

    info niscope

--- PAandQubit/Instruments/Acquisition_Card/Card_Class.py
+++ PAandQubit/Instruments/Acquisition_Card/Card_Class.py
@@ -3,16 +3,12 @@
 ##### https://niscope.readthedocs.io/en/latest/class.html#read ######
 
 import niscope as ni
-import time
-
-
-
 
 class PXIe5170R:
 
-    def __init__(self, dev_name: str):
+    def __init__(self, resource_name: str = 'PXI1Slot2'):
 
-        self._dev_name          = dev_name
+        self._resource_name     = resource_name
         self._voltage_range     = 1
         self._coupling          = 'DC'
         self._sample_rate       = int(250e6)
@@ -32,11 +28,17 @@ class PXIe5170R:
             print("Connection successful!")
         except:
             print(f"Unable to establish a connection.")
-    def voltag_range:
-    def coupling:
-    def sample_rate:
-    def num_pts:
-    def num_records:
+
+
+    def voltag_range(self, voltage_range):
+        self._voltage_range = voltage_range
+
+
+
+    def coupling():
+    def sample_rate():
+    def num_pts():
+    def num_records():
     def fetch(self, ):
         with ni.Session(self._dev_name) as session: # Name of the device
         session.channels[0].configure_vertical(range = voltage_range, coupling=ni.VerticalCoupling.AC)
