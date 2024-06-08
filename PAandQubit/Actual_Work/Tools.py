b55232bcf68c20a6bc39ad9ddeb5ee6c81c779ab
Finardi-FS <fs.finardi@gmail.com>
2024-06-08 15:32:40 +0200

    Cables

--- PAandQubit/Actual_Work/Tools.py
+++ PAandQubit/Actual_Work/Tools.py
@@ -1,12 +1,34 @@
 import numpy as np
 
 
-def dBm_to_mV(dbm, R = 50 ):                            # R = 50 Ohm
+def dBm_to_mVrms(dbm, R = 50):                            # R = 50 Ohm
+
+    return (np.sqrt(R * 10**((dbm - 30)/10)))* 1e3
+
+
+
+def mVrms_to_dBm(mv, R = 50):
+
+    return 10 * np.log10((mv * 1e-3)**2 * 1000 / R)
+
+
+def dBm_to_mVpk(dbm, R = 50):                            # R = 50 Ohm
 
     return (np.sqrt(2 * R * 10**((dbm - 30)/10)))* 1e3
 
 
 
-def mV_to_dBm(mv, R = 50):
+def mVpk_to_dBm(mv, R = 50):
 
-    return 10 * np.log10(((mv*1e-3)**2)*500/ R)
+    return 10 * np.log10((mv * 1e-3)**2 * 500 / R)
+
+
+def dBm_to_mVpp(dbm, R = 50):                            # R = 50 Ohm
+
+    return 2 * (np.sqrt(2 * R * 10**((dbm - 30)/10)))* 1e3
+
+
+
+def mVpp_to_dBm(mv, R = 50):
+
+    return 10 * np.log10((mv * 1e-3)**2 * 125 / R)
