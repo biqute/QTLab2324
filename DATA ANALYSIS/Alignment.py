45e0707a4a442611ed4d7561f21cd52cd10b4831
Richardforphys <r.maifredi@campus.unimib.it>
2024-08-09 19:32:33 +0200

    Now aligned data will be uploaded on Google Drive

--- DATA ANALYSIS/Alignment.py
+++ DATA ANALYSIS/Alignment.py
@@ -1,13 +1,15 @@
 from UTILS.load_data import get_data, in_memory
-from UTILS import drive
-from UTILS.Trigger import derivative_trigger, live_show
+from UTILS.drive import compress_hdf5, upload_file_to_drive, create_hdf5_file, authenticate
+from UTILS.Trigger import derivative_trigger, live_show, align_signal
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import os
 import multiprocessing
 
 def main():
-    files, svc = get_data(max_files=300)
+    
+    files, svc = get_data(max_files=1000)
 
     # Start live_show in a separate process
     live_show_process = multiprocessing.Process(target=live_show, args=('Alignments.txt',))
@@ -38,7 +40,27 @@ def main():
         with open('Alignments.txt', 'a') as file:
             file.writelines(str(b) + '\n')
 
-    # Ensure the live_show process completes
+        final, _ = align_signal(data,100,1000,20000)
+
+        hdf5_file_path = 'data.hdf5'  # Path for the HDF5 file to create
+        compressed_file_path = 'data.hdf5.gz'  # Path for the compressed file
+
+        # Create HDF5 file
+        create_hdf5_file(hdf5_file_path,final)
+
+        # Compress HDF5 file
+        compress_hdf5(hdf5_file_path, compressed_file_path)
+
+        upload_folder_id = '19ApmRMhBMmQ7gT2i8akhViuKDyhP3QML'
+
+        # Upload to Google Drive
+        upload_file_to_drive(compressed_file_path, upload_folder_id)
+
+        # Clean up
+        os.remove(hdf5_file_path)
+        os.remove(compressed_file_path)
+
+        # Ensure the live_show process completes
     live_show_process.join()
 
 if __name__ == "__main__":
--- DATA ANALYSIS/UTILS/drive.py
+++ DATA ANALYSIS/UTILS/drive.py
@@ -3,16 +3,20 @@ import io
 import sys
 sys.path.append(r'C:\Users\ricca\Desktop\MAGISTRALE\QTLab2324\IR_SING_PHOT')
 from HDF5 import HDF5
+from googleapiclient.http import MediaFileUpload
 import pickle
 import h5py
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaIoBaseDownload
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+import h5py
+import os
+import gzip
+import shutil
 
 # If modifying these SCOPES, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def authenticate():
     """Authenticate the user and return the service."""
@@ -26,8 +30,9 @@ def authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(r'C:\Users\ricca\Desktop\MAGISTRALE\QTLab2324\DATA ANALYSIS\UTILS\credentials.json', SCOPES)
-            creds = flow.run_local_server(port=49901)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                r'C:\Users\ricca\Desktop\MAGISTRALE\QTLab2324\DATA ANALYSIS\UTILS\credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080)  # Set port=0 to choose an available port
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
@@ -90,3 +95,30 @@ def read_hdf5_file(file_stream):
         # Assuming 'Signals/I' is the correct path inside the HDF5 structure
         dataset = f['Signals']['I'][:]
     return dataset
+
+def create_hdf5_file(file_path,data):
+    """Create a sample HDF5 file with some example data."""
+    with h5py.File(file_path, 'w') as hdf5_file:
+        # Create a dataset with random data
+        hdf5_file.create_dataset('example_data', data=data)
+    print(f'HDF5 file created at {file_path}')
+
+def compress_hdf5(file_path, output_path):
+    """Compress HDF5 file using gzip."""
+    with open(file_path, 'rb') as f_in:
+        with gzip.open(output_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+def upload_file_to_drive(file_path, drive_folder_id=None):
+    """Upload a file to Google Drive."""
+    service = authenticate()  # Get the Google Drive service
+    
+    file_name = os.path.basename(file_path)
+    file_metadata = {'name': file_name}
+    if drive_folder_id:
+        file_metadata['parents'] = [drive_folder_id]
+    
+    media = MediaFileUpload(file_path, resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    
+    print(f'File {file_name} uploaded to Google Drive with file ID: {file["id"]}')
